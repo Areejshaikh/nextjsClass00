937283738837fe5fa2eb235da84f1453748ff970
Areejshaikh <160318564+Areejshaikh@users.noreply.github.com>
2024-10-15 22:58:16 -0700

    initial commit

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="text-center text-4xl mt-72 underline text-slate-600">
+      <h1>
+      Welcome Areej Zaheer!</h1>
+    </div>
+  );
+}
